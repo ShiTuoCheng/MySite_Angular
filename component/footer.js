7e730791728895f3f945d3c78a007a54a9c56013
Shi Tuocheng <shituocheng@126.com>
2017-03-27 18:52:53 +0800

    Initial commit

---
+++ component/footer.js
@@ -0,0 +1,91 @@
+var app = angular.module('mySite');
+
+app.directive('mFooter', function(){
+    return{
+        restrict:'E',
+        replace: true,
+        template:'<footer class="mFooter">'+
+                    '<div class="wrapper">'+
+                        '<div class="timing">{{currentTime}}</div>'+
+                        '<div class="weatherDiv">{{location}} | {{weatherState}} | {{weatherTemp}}</div>'+
+                        '<div class="content">'+
+                            '<span>Powered by </span>'+
+                            '<img src="../../images/angular.jpg"/>'+
+                        '</div>'+
+                    '</div>'+
+                 '</footer>',
+
+        controller: function($scope, $interval, $timeout, $http){
+
+// 更新时间
+            $scope.currentTime = getTime();
+
+            getIP();
+
+            var updateClock = function() {
+                $scope.currentTime = getTime();
+            };
+            $interval(updateClock, 1000);
+
+            //获取时间
+            function getTime(){
+                var date = new Date();
+
+                return date.getFullYear()+ "年" + formateNum(date.getMonth()+1)+ "月" + formateNum(date.getDate()) + "日" + " 星期" + formateDay(date.getDay()) + " " + formateNum(date.getHours()) + ":" + formateNum(date.getMinutes()) + ":" + formateNum(date.getSeconds());
+            };
+
+// 格式化数字
+            function formateNum(num){
+                if (num < 10){
+
+                    return "0" + num;
+                }else{
+          
+                    return num;
+                }
+            };
+
+// 格式化时间
+            function formateDay(day){
+                if (typeof day  === "number") {
+                    if (day === 1){
+                        return "一";
+                    }else if(day === 2){
+                        return "二";
+                    }else if(day === 3){
+                        return "三";
+                    }else if(day === 4){
+                        return "四";
+                    }else if(day === 5){
+                        return "五";
+                    }else if(day === 6){
+                        return "六";
+                    }else if(day === 7){
+                        return "七";
+                    }
+                }
+            };
+
+            //获取ip地址
+            function getIP(){
+
+                $scope.location = '位置';
+                $scope.weatherState = '天气情况';
+                $scope.weatherTemp = '温度';
+
+                $.getJSON("http://jsonip.com/?callback=?", (data) => {
+                    setWeather(data.ip);
+                });
+            };
+
+            //获取天气
+            function setWeather(ipAddress){
+                $.getJSON("https://free-api.heweather.com/v5/now?city="+ipAddress+"&key=56cb41b979f944b58c96e98c457a2f5b", (data) => {
+                    $scope.location = data.HeWeather5[0].basic.city;
+                    $scope.weatherState = data.HeWeather5[0].now.cond.txt;
+                    $scope.weatherTemp = data.HeWeather5[0].now.tmp + '℃';
+        });
+            }
+        }
+    }
+});
---
+++ component/header.js
@@ -0,0 +1,42 @@
+var app = angular.module('mySite', ['ngRoute']);
+
+app.directive('mHeader', function(){
+    return{
+        restrict:'E',
+        replace: true,
+        template:'<header class="mHeader">'+
+                    '<div class="wrapper">'+
+                        '<div class="avatarDiv"></div>'+
+                        '<span class="title">Welcome to my world!!</span>'+
+                        '<ul>'+
+                            '<li ng-repeat="item in titleItems">{{test($index)}}<a href="{{item.url}}">{{item.title}}</a><div class="selectBar"></div></li>'+
+                        '</ul>'+
+                        ''+
+                    '</div>'+
+                 '</header>',
+        controller: function($scope, $location){
+
+            $scope.titleItems = [
+                {'title':'主页', 'url':'#/', isClass: "/"},
+                {'title':'我的文章', 'url':'#/article', isClass: "/article"},
+                {'title':'开发项目~', 'url':'#/post', isClass: "/post"},
+                {'title':'关于我', 'url':'#/aboutMe', isClass: "/aboutMe"}
+            ];
+
+            $scope.test = function($index){
+                var url = $location.url();
+                ($scope.titleItems[$index].isClass === url) ? $('ul > li').eq($index).addClass('active'): $('ul > li').eq($index).removeClass('active');
+            }
+        }
+    }
+}).config(['$routeProvider', function($routeProvider){
+                $routeProvider
+                .when('/',{templateUrl:'./index.html'})
+                .when('/article',{template:'我的文章'})
+                .when('/post',{template:'开发项目'})
+                .when('/aboutMe',{template:'关于我'})
+                .otherwise({redirectTo:'/'})
+            }
+]).config(['$locationProvider', function($locationProvider) {
+                $locationProvider.hashPrefix('');
+}]);
