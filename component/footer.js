f7bd3b920d7abe6f57a0e5e3da32834df9e98880
Shi Tuocheng <shituocheng@126.com>
2017-03-28 13:49:38 +0800

    add some code

--- component/footer.js
+++ component/footer.js
@@ -1,6 +1,6 @@
 var app = angular.module('mySite');
 
-app.directive('mFooter', function(){
+app.directive('mFooter', function(footerTimeFac, footerWeatherFac){
     return{
         restrict:'E',
         replace: true,
@@ -17,18 +17,42 @@ app.directive('mFooter', function(){
 
         controller: function($scope, $interval, $timeout, $http){
 
-// 更新时间
-            $scope.currentTime = getTime();
+            //视图界面初始化
+            $scope.currentTime = footerTimeFac.getTime();
+            $scope.location = '位置';
+            $scope.weatherState = '天气情况';
+            $scope.weatherTemp = '温度';
 
             getIP();
-
+// 更新时间
             var updateClock = function() {
-                $scope.currentTime = getTime();
+                $scope.currentTime = footerTimeFac.getTime();
             };
             $interval(updateClock, 1000);
 
-            //获取时间
-            function getTime(){
+            //获取ip地址
+            function getIP(){
+
+                $.getJSON("http://jsonip.com/?callback=?", (data) => {
+                    setWeather(data.ip);
+                });
+            };
+
+            //获取天气
+            function setWeather(ipAddress){
+                $.getJSON("https://free-api.heweather.com/v5/now?city="+ipAddress+"&key=56cb41b979f944b58c96e98c457a2f5b", (data) => {
+                    $scope.location = data.HeWeather5[0].basic.city;
+                    $scope.weatherState = data.HeWeather5[0].now.cond.txt;
+                    $scope.weatherTemp = data.HeWeather5[0].now.tmp + '℃';
+                });
+            }
+        }
+    }
+}).service('footerTimeFac', function(){
+
+            var time = "";
+ //获取时间
+            this.getTime =function(){
                 var date = new Date();
 
                 return date.getFullYear()+ "年" + formateNum(date.getMonth()+1)+ "月" + formateNum(date.getDate()) + "日" + " 星期" + formateDay(date.getDay()) + " " + formateNum(date.getHours()) + ":" + formateNum(date.getMinutes()) + ":" + formateNum(date.getSeconds());
@@ -65,27 +89,6 @@ app.directive('mFooter', function(){
                     }
                 }
             };
-
-            //获取ip地址
-            function getIP(){
-
-                $scope.location = '位置';
-                $scope.weatherState = '天气情况';
-                $scope.weatherTemp = '温度';
-
-                $.getJSON("http://jsonip.com/?callback=?", (data) => {
-                    setWeather(data.ip);
-                });
-            };
-
-            //获取天气
-            function setWeather(ipAddress){
-                $.getJSON("https://free-api.heweather.com/v5/now?city="+ipAddress+"&key=56cb41b979f944b58c96e98c457a2f5b", (data) => {
-                    $scope.location = data.HeWeather5[0].basic.city;
-                    $scope.weatherState = data.HeWeather5[0].now.cond.txt;
-                    $scope.weatherTemp = data.HeWeather5[0].now.tmp + '℃';
-        });
-            }
-        }
-    }
+}).service('footerWeatherFac', function(){
+    
 });
